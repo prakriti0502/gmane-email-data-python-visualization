9ca9c396c05e997192dec5c99bd2165d5d595204
prakriti0502 <prakriti.sagar@icloud.com>
2020-06-28 20:34:08 +0530

    gmane done

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','ufp.pt','mac.com','indiana.edu','berkeley.edu','uct.ac.za','columbia.edu','mtu.edu','gmail.com'],
+['2005-12',56,11,10,11,12,11,14,13,11,10],
+['2006-01',5,7,7,3,2,3,0,0,0,1]
+];
